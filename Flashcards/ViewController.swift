89e3414d235b9efdd29ff8a35e9e5e577b83aea1
Jeanette Lin <jeanettelin@jeanettes-macbook-pro.local>
2018-10-13 13:56:43 -0700

    Simple Flashcards Lab

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -20,6 +20,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBOutlet weak var questionLabel: UILabel!
+    
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBAction func didTapOnFlashcard(_ sender: Any) {
+        questionLabel.isHidden = true
+        
+    }
+    
 }
 
